2e0619d8b00e1adb8a8b4f5dcc68873764295248
shlomo1839@gmail.com <shlomoelias1830>
2026-02-02 07:31:04 +0200

    finish

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const input = document.getElementById("sizebox");
-const btn = document.getElementById("box");
+const btn = document.getElementById("btn");
 const container = document.getElementById("container")
 
 btn.addEventListener('click', function() {
     const count = input.value
     container.innerHTML = "";
 
-    for (let i = 1; i <= 0; i++){
+    for (let i = 1; i <= count; i++){
         const newBox = document.createElement('div');
         newBox.classList.add('box')
         newBox.innerText = i;
 
 
-         const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+        const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
         newBox.style.backgroundColor = randomColor;
 
         newBox.addEventListener('click', function() {
